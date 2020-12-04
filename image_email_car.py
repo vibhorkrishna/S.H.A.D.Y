77f03f2defe80ea9355a750af513b57572009cef
Vibhor Krishna <vibhorkishna98@gmail.com>
2020-12-05 00:20:51 +0530

    Made some changes for sending alert message for car crash

---
+++ image_email_car.py
@@ -0,0 +1,26 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+def SendMail(ImgFileName):
+    img_data = open(ImgFileName, 'rb').read()
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Crash Alert'
+    msg['From'] = 'sample@gmail.com.cc'
+    msg['To'] = 'sample@gmail.com.cc'
+
+    text = MIMEText("Vehicle Crash Detected. Send HELP")
+    msg.attach(text)
+    image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
+    msg.attach(image)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login("sample_sender@gmail.com", "sample_password")
+    s.sendmail("sample_sender@gmail.com", "sample_receiver@gmail.com", msg.as_string())
+    s.quit()
